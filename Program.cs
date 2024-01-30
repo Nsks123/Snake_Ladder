853a6508296a89f6b35286d8d45f58ccf5bf7711
Nsks123 <nallamsunilkumar5@gmail.com>
2024-01-30 15:25:12 +0530

    [sunil] Done:Functionality till the player reaches the winning position

--- Program.cs
+++ Program.cs
@@ -10,24 +10,37 @@ namespace Snake_Ladder
     {
         static void Main(string[] args)
         {
-            int position = 0;
             Random rnd = new Random();
-            int number=rnd.Next(1,7);
-            int option=rnd.Next(0,3);
-            switch (option)
+            int position = 0;
+            int last_position = 100;
+            
+            while(position<=last_position) 
             {
-                case 0:
-                    Console.WriteLine("The player stays in the same position");
-                    break;
-                case 1:
-                    Console.WriteLine($"The player moves ahead = {position} + {number}" );
-                    break;
-                case 2:
-                    Console.WriteLine($"The player moves behind = {position} - {number}" );
-                    break;
+                int number = rnd.Next(1, 7);
+                int option = rnd.Next(0, 3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("The player stays in the same position");
+                        break;
+                    case 1:
+                        position += number;
+                        Console.WriteLine($"The player moves ahead = {position}");
+                        break;
+                    case 2:
+                        position -= number;
+                        if (position < 0)
+                        {
+                            position = 0;
+
+                        }
+                        Console.WriteLine($"The player moves behind = {position}");
+                        break;
+
+                }
+                
             }
-            Console.WriteLine($"player rolls the dice to get a number = {number}");
-            Console.WriteLine($"current position of player = {number}");
+           
             Console.ReadLine();
            
         }
