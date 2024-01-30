36a7c3ab29e45bdc7f761b254815102526fc4240
Nsks123 <nallamsunilkumar5@gmail.com>
2024-01-30 15:06:37 +0530

    [sunil] Done: Functionality to check whether player stays at current position or moves ahead or moves behind

--- Program.cs
+++ Program.cs
@@ -10,9 +10,24 @@ namespace Snake_Ladder
     {
         static void Main(string[] args)
         {
+            int position = 0;
             Random rnd = new Random();
             int number=rnd.Next(1,7);
+            int option=rnd.Next(0,3);
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("The player stays in the same position");
+                    break;
+                case 1:
+                    Console.WriteLine($"The player moves ahead = {position} + {number}" );
+                    break;
+                case 2:
+                    Console.WriteLine($"The player moves behind = {position} - {number}" );
+                    break;
+            }
             Console.WriteLine($"player rolls the dice to get a number = {number}");
+            Console.WriteLine($"current position of player = {number}");
             Console.ReadLine();
            
         }
